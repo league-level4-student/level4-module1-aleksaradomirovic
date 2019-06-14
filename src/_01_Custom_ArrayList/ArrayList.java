41b0dedb7594ad601a671c731756a28823f6112c
League Student <league@iMac-4.local>
2019-06-13 19:58:21 -0700

    did stuff

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,33 +3,63 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
+	T[] list;
 	
 	public ArrayList() {
+		list = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return list[loc];
 	}
 	
 	public void add(T val) {
+		T[] temp = (T[]) new Object[list.length+1];
+		for(int i = 0; i < list.length; i++) {
+			temp[i] = list[i];
+		}
+		temp[list.length] = val;
 		
+		list = temp;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
+		T[] temp = (T[]) new Object[list.length+1];
+		for(int i = 0; i < loc; i++) {
+			temp[i] = list[i];
+		}
+		temp[loc] = val;
+		for(int i = loc; i < list.length; i++) {
+			temp[i+1] = list[i];
+		}
 		
+		list = temp;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		list[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+		T[] temp = (T[]) new Object[list.length-1];
+		for(int i = 0; i < list.length-1; i++) {
+			if(i < loc)
+				temp[i] = list[i];
+			else if(i > loc)
+				temp[i-1] = list[i];
+		}
 		
+		list = temp;
 	}
 	
 	public boolean contains(T val) {
-		
+		for(T e : list) {
+			if(val == e) return true;
+		}
 		return false;
+	}
+	
+	public int size() {
+		return list.length;
 	}
 }
